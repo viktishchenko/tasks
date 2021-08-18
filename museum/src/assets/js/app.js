eae1e7ab29b613b6364e42138b97c8ddf0cd05a2
Viktor Tishchenko <9043258451vvt@gmail.com>
2021-08-18 09:11:06 +0600

    feat: add video journey section

--- museum/src/assets/js/app.js
+++ museum/src/assets/js/app.js
@@ -1,2 +1,3 @@
 //= ./components/modal.js
+//= ./components/player.js
 console.log("Hello from appjs");
--- museum/src/assets/js/components/player.js
+++ museum/src/assets/js/components/player.js
@@ -1,8 +1,6 @@
-console.log("Hello from modal");
 const progress = document.querySelector(".progress");
 
 progress.addEventListener("input", function () {
   const value = this.value;
   this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #fff ${value}%, white 100%)`;
 });
-console.log("Hello from appjs");
