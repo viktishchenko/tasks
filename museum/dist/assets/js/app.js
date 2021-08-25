2a656fda9bbaa3d2a420639fd73292b261798a15
Viktor Tishchenko <9043258451vvt@gmail.com>
2021-08-25 19:48:29 +0600

    feat: add gallery section with PP

--- museum/dist/assets/js/app.js
+++ museum/dist/assets/js/app.js
@@ -3,6 +3,6 @@ const progress = document.querySelector(".progress");
 
 progress.addEventListener("input", function () {
   const value = this.value;
-  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #fff ${value}%, white 100%)`;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`;
 });
 console.log("Hello from appjs");
--- museum/src/assets/js/components/player.js
+++ museum/src/assets/js/components/player.js
@@ -2,5 +2,5 @@ const progress = document.querySelector(".progress");
 
 progress.addEventListener("input", function () {
   const value = this.value;
-  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #fff ${value}%, white 100%)`;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`;
 });
