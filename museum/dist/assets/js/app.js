9e9638687500aacbb0bb556b416c96e8fe23abce
Viktor Tishchenko <9043258451vvt@gmail.com>
2021-09-25 07:30:31 +0600

    fix: main page PP css-styles

--- museum/dist/assets/js/app.js
+++ museum/dist/assets/js/app.js
@@ -33,7 +33,6 @@ const bookingInputs = document.querySelectorAll(".form__input");
 for (el of bookingInputs) {
   el.addEventListener("click", function () {
     const labelName = this.dataset.label;
-    console.log(labelName);
     document.getElementById(labelName).classList.add("label-hide");
   });
 }
